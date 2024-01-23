93ef5849fade7d840d1b1f995bc27e73625ca1cf
Gamze <gamzecoskkun@gmail.com>
2024-01-23 23:24:15 +0300

    Dependencies Added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.com.google.devtools.kts) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.navigation.safeargs) apply false
+}
+buildscript {
+    dependencies {
+        classpath(libs.navigationSafeArgs)
+    }
 }
 true // Needed to make the Suppress annotation work for the plugins block
